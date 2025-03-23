270c0f7d64f9ab33fb6adbe2873fd820f3705753
ArtemAntoshchenko <fgd19611@yndex.ru>
2025-03-23 18:46:14 +0300

    second commit

--- Стрелочные функции.js
+++ Стрелочные функции.js
@@ -11,3 +11,32 @@ console.log(greet(prompt('Введите ваше имя')))
 let max=(x,y)=>Math.max(x,y)
 console.log(max(6,4))
 
+let i=2
+let isEven=(+prompt('Введите число')%i==0) ?
+    ()=>console.log("true"):
+    ()=>console.log("false");
+isEven()
+    
+let reverseString=(string)=>console.log(string.split("").reverse().join(''))
+reverseString("Hello")
+
+let factorial=(factorial1)=> {
+    if (factorial1==0) {
+        return 1;
+    } else {
+        return factorial1*factorial(factorial1-1);
+    }
+}
+console.log(factorial(+prompt('Введите число:')))
+
+
+function createCounter() {
+    let counter=0;
+    return {
+        increment:()=>counter+1,
+        decrement:()=>counter-1
+    }
+}
+let count=createCounter()
+console.log(count.increment())
+console.log(count.decrement())
